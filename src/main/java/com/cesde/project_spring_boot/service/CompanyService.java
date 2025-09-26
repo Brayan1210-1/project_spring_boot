74400d24e923d7c4b1c035ff68e69edcbf058932
Brayan Quebrada <alexisquebrada11@gmail.com>
2025-09-26 18:03:20 -0500

    feat(company):companyservice-companycontroller-added

--- src/main/java/com/cesde/project_spring_boot/service/CompanyService.java
+++ src/main/java/com/cesde/project_spring_boot/service/CompanyService.java
@@ -1,8 +1,10 @@
 package com.cesde.project_spring_boot.service;
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.cesde.project_spring_boot.dto.CompanyDTO;
@@ -14,11 +16,16 @@ import com.cesde.project_spring_boot.repository.UserRepository;
 
 
 
+
+
 @Service
 public class CompanyService {
 	
 	@Autowired
     private CompanyRepository companyRepository;
+	
+	@Autowired 
+	private UserRepository userRepository;
 
 	public List<CompanyDTO> getAllCompannies() {
         List<Company> companys = companyRepository.findAll();
@@ -44,7 +51,7 @@ public class CompanyService {
 	 public CompanyDTO createCompany(CompanyDTO companyDTO) {
 	        // Validar que el email no exista
 	        if (companyRepository.existsByEmail(companyDTO.getEmail())) {
-	            throw new RuntimeException("Ya existe un usuario con el email: " + companyDTO.getEmail());
+	            throw new RuntimeException("Ya existe una empresa con el email: " + companyDTO.getEmail());
 	        }
 
 	        // Convertir DTO a entidad y guardar
@@ -78,25 +85,59 @@ public class CompanyService {
 	        }
 	 }
 	 
+	 public void deleteCompany(Long companyId) {
+	        if (companyRepository.existsById(companyId)) {
+	            companyRepository.deleteById(companyId);
+	        } else {
+	            throw new RuntimeException("Empresa no encontrada con ID: " + companyId);
+	        }
+	    }
+
+	 public List<User> addEmployee( UserDTO employeeDTO, long companyId){
 	 
-	/*
-	 * * public ArrayList<User> agregarEmpleado(CompanyDTO company, UserDTO employee, long id){
-	 
-	 
-		 UserRepository userRepository;
-		 Optional<User> employeeOptional = userRepository.findById(id);
-		 Optional<Company> companyOptional = companyRepository.findById(id);
-		 if (companyOptional.isPresent()) {
-			 
-	            Company companie = companyOptional.get();
-         
-	            //verificar que el id del usuario no se repita
-	            if (employee.getId().intValue(employeeOptional.()) &&
-	                    companyRepository.existsByEmail(companyDTO.getEmail())) {
-	                throw new RuntimeException("Ya existe una empresa con el email: " + companyDTO.getEmail());
-	            }
+		 Optional<Company> companyOptional = companyRepository.findById(companyId);
+		 
+		//verificar que la empresa exista
+		 if (!companyOptional.isPresent()) {
+			 throw new RuntimeException("Empresa no encontrada con ID: " + companyId);
 		 }
-		 return null;
-	 }*/
+		    
+		 Company company = companyOptional.get();
+		 
+		 
+		    User employee = employeeDTO.toEntity();
+		           
+		    
+		    User savedEmployee = userRepository.save(employee);
+
+	        //añadir empleado a la lista 
+	        company.addEmployee(savedEmployee);
+
+	        // Guardar la empresa
+	        companyRepository.save(company);
+
+	        // Retornar lista de empleados
+	        return company.getEmployees();
+	 }
 	    
+	 public void deleteEmployeeById(CompanyDTO companyDTO, long empleadoId) {
+		 Optional<Company> companyOptional = companyRepository.findById(companyDTO.getId());
+		 
+		 //saber si empresa existe
+		 if (!companyOptional.isPresent()) {
+			 throw new RuntimeException("Empresa no encontrada con ID: " + companyDTO.getId());
+		 }
+		 
+		 
+		 User employeeToEliminate = (User) companyDTO.getEmployees().stream().filter(user -> user.getId() == empleadoId);
+		 //Verificar si el empleado existe
+		 if (employeeToEliminate == null) {
+			 throw new RuntimeException("Usuario no encontrado con ID: " + empleadoId);
+		 }
+		 
+		
+		 
+		Company company = companyOptional.get();
+		company.removeEmployee(employeeToEliminate);
+	 }
 }
